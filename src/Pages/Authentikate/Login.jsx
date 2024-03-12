c2482971d8ccb792f621379561b880f38897dd62
unknown <atikrahad1@gmail.com>
2024-03-12 17:43:26 +0600

    adding login page design

---
+++ src/Pages/Authentikate/Login.jsx
@@ -0,0 +1,94 @@
+import { Link } from "react-router-dom";
+import Button from "../../Components/Button";
+import logo from "../../assets/Authentication/graphingsine.mp4";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+import { useState } from "react";
+
+const Login = () => {
+  const [isOpen, setIsopen] = useState(false)
+    const handleLogin = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email,password);
+  };
+  return (
+    <div className=" flex items-center justify-center min-h-screen">
+      <div className="max-w-screen-2xl md:max-h-screen overflow-hidden flex items-center mx-auto bg-[#f3f3f3]">
+        <div className="px-10">
+          <div className="hero-content flex-col ">
+            <div className="text-center lg:text-left">
+              <h1 className="text-4xl font-bold">Login now!</h1>
+            </div>
+            <div className="card shrink-0 w-full max-w-sm ">
+              <form onSubmit={handleLogin} className="card-body">
+                <div className="form-control">
+                  <label className="input input-bordered rounded-sm bg-transparent flex items-center gap-2">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 16 16"
+                      fill="currentColor"
+                      className="w-4 h-4 opacity-70"
+                    >
+                      <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+                      <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+                    </svg>
+                    <input
+                      type="text"
+                      name="email"
+                      className="grow"
+                      placeholder="Email"
+                    />
+                  </label>
+                </div>
+                <div className="form-control">
+                  <label className="input input-bordered rounded-sm bg-transparent flex items-center gap-2">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 16 16"
+                      fill="currentColor"
+                      className="w-4 h-4 opacity-70"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                    <input
+                      type={isOpen? "text": "password"}
+                      name="password"
+                      className="grow"
+                      placeholder="Password"
+                    />
+                    <button onClick={()=>setIsopen(!isOpen)} className="absolute text-xl right-10">
+                        {
+                            isOpen? <AiOutlineEye /> : <AiOutlineEyeInvisible/>
+                        }
+                      
+                    </button>
+                  </label>
+                </div>
+
+                <div className="form-control mt-6">
+                  <button className="">
+                    <Button title={"Login"}></Button>
+                  </button>
+                </div>
+                <p>
+                  New user? <Link to="register">Register</Link>
+                </p>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div className="hidden md:contents">
+          <video className="h-full" src={logo} muted loop autoPlay></video>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- src/Router/router.jsx
+++ src/Router/router.jsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Landingpage from "../Layout/Landingpage/Landingpage";
+import Login from "../Pages/Authentikate/Login";
 
 
 const router = createBrowserRouter([
@@ -7,6 +8,10 @@ const router = createBrowserRouter([
       path: "/",
       element: <Landingpage></Landingpage>,
     },
+    {
+      path: "/login",
+      element: <Login></Login>
+    }
   ]);
 
 export default router;
