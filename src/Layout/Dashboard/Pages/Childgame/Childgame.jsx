001b4fe6edf1682ce98e0ac6b21d89762a7d3823
Atik Rahad <atikrahad1@gmail.com>
2024-05-12 10:02:08 +0600

    adding subtract

--- src/Layout/Dashboard/Pages/Childgame/Childgame.jsx
+++ src/Layout/Dashboard/Pages/Childgame/Childgame.jsx
@@ -1,41 +1,64 @@
-import React, { useEffect, useState } from "react";
+import Add from "./Components/Add";
+import Subtract from "./Components/Subtract";
 
 const Childgame = () => {
-  const [arr, setArr] = useState([]);
-  const [firstv, setFirstv] = useState(0);
-  const [sec, setSec] = useState(0);
-
-  useEffect(() => {
-    const firstValue = Math.ceil(Math.random() * 20);
-    const secondValue = Math.ceil(Math.random() * 20);
-    const first = Math.ceil(Math.random() * 40);
-    const second = Math.ceil(Math.random() * 40);
-    const ans = firstValue + secondValue;
-    setFirstv(firstValue);
-    setSec(secondValue);
-    setArr([first, second, ans]);
-  }, []);
-  console.log(arr);
-
-  const output = arr.sort(() => Math.random() - 0.5);
-
   return (
-    <div className="grid grid-cols-10 gap-5 max-w-screen-xl mx-auto my-10">
-      <div className="col-span-7 h-[80vh] shadow-xl rounded-md badge-ghost">
-        <div className="w-full flex items-center justify-center h-full">
-          <h1 className="text-[9em] font-bold">
-            {firstv} + {sec} = ?
-          </h1>
-        </div>
+    <div role="tablist" className="tabs max-w-screen-xl mx-auto py-5 tabs-lifted">
+      <input
+        type="radio"
+        name="my_tabs_2"
+        role="tab"
+        className="tab text-2xl"
+        aria-label="Addition"
+        checked
+      />
+      <div
+        role="tabpanel"
+        className="tab-content bg-base-100 border-base-300 rounded-box p-6"
+      >
+        <Add/>
       </div>
-      <div className="col-span-3 gap-5 grid grid-rows-3">
-        {output.map((i) => (
-          <div className="badge-ghost shadow-xl rounded-md">
-            <div className="w-full h-full flex items-center justify-center">
-              <h1 className="text-7xl font-semibold">{i}</h1>
-            </div>
-          </div>
-        ))}
+
+      <input
+        type="radio"
+        name="my_tabs_2"
+        role="tab"
+        className="tab text-2xl"
+        aria-label="Substract"
+        
+      />
+      <div
+        role="tabpanel"
+        className="tab-content bg-base-100 border-base-300 rounded-box p-6"
+      >
+        <Subtract/>
+      </div>
+
+      <input
+        type="radio"
+        name="my_tabs_2"
+        role="tab"
+        className="tab text-2xl"
+        aria-label="Multiply"
+      />
+      <div
+        role="tabpanel"
+        className="tab-content bg-base-100 border-base-300 rounded-box p-6"
+      >
+        Tab content 3
+      </div>
+      <input
+        type="radio"
+        name="my_tabs_2"
+        role="tab"
+        className="tab text-2xl"
+        aria-label="Divition"
+      />
+      <div
+        role="tabpanel"
+        className="tab-content bg-base-100 border-base-300 rounded-box p-6"
+      >
+        Tab content 3
       </div>
     </div>
   );
