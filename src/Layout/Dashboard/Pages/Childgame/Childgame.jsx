501b335787671a491959699442095b590fcced9d
Atik Rahad <atikrahad1@gmail.com>
2024-05-12 10:58:35 +0600

    adding multiply and divide

--- src/Layout/Dashboard/Pages/Childgame/Childgame.jsx
+++ src/Layout/Dashboard/Pages/Childgame/Childgame.jsx
@@ -1,64 +1,71 @@
 import Add from "./Components/Add";
+import Divide from "./Components/Divide";
+import Multiply from "./Components/Multiply";
 import Subtract from "./Components/Subtract";
 
 const Childgame = () => {
   return (
-    <div role="tablist" className="tabs max-w-screen-xl mx-auto py-5 tabs-lifted">
-      <input
-        type="radio"
-        name="my_tabs_2"
-        role="tab"
-        className="tab text-2xl"
-        aria-label="Addition"
-        checked
-      />
+    <div>
+        <h1 className="text-center mt-5">Double click to switch another tab</h1>
       <div
-        role="tabpanel"
-        className="tab-content bg-base-100 border-base-300 rounded-box p-6"
+        role="tablist"
+        className="tabs max-w-screen-xl mx-auto py-5 tabs-lifted"
       >
-        <Add/>
-      </div>
+        <input
+          type="radio"
+          name="my_tabs_2"
+          role="tab"
+          className="tab text-2xl"
+          aria-label="Addition"
+          checked
+        />
+        <div
+          role="tabpanel"
+          className="tab-content bg-base-100 border-base-300 rounded-box p-6"
+        >
+          <Add />
+        </div>
 
-      <input
-        type="radio"
-        name="my_tabs_2"
-        role="tab"
-        className="tab text-2xl"
-        aria-label="Substract"
-        
-      />
-      <div
-        role="tabpanel"
-        className="tab-content bg-base-100 border-base-300 rounded-box p-6"
-      >
-        <Subtract/>
-      </div>
+        <input
+          type="radio"
+          name="my_tabs_2"
+          role="tab"
+          className="tab text-2xl"
+          aria-label="Substract"
+        />
+        <div
+          role="tabpanel"
+          className="tab-content bg-base-100 border-base-300 rounded-box p-6"
+        >
+          <Subtract />
+        </div>
 
-      <input
-        type="radio"
-        name="my_tabs_2"
-        role="tab"
-        className="tab text-2xl"
-        aria-label="Multiply"
-      />
-      <div
-        role="tabpanel"
-        className="tab-content bg-base-100 border-base-300 rounded-box p-6"
-      >
-        Tab content 3
-      </div>
-      <input
-        type="radio"
-        name="my_tabs_2"
-        role="tab"
-        className="tab text-2xl"
-        aria-label="Divition"
-      />
-      <div
-        role="tabpanel"
-        className="tab-content bg-base-100 border-base-300 rounded-box p-6"
-      >
-        Tab content 3
+        <input
+          type="radio"
+          name="my_tabs_2"
+          role="tab"
+          className="tab text-2xl"
+          aria-label="Multiply"
+        />
+        <div
+          role="tabpanel"
+          className="tab-content bg-base-100 border-base-300 rounded-box p-6"
+        >
+          <Multiply />
+        </div>
+        <input
+          type="radio"
+          name="my_tabs_2"
+          role="tab"
+          className="tab text-2xl"
+          aria-label="Divition"
+        />
+        <div
+          role="tabpanel"
+          className="tab-content bg-base-100 border-base-300 rounded-box p-6"
+        >
+          <Divide />
+        </div>
       </div>
     </div>
   );
